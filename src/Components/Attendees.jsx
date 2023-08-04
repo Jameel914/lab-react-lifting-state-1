3eee6ce1b1c024bc59dacf87da568d643170d688
jameel914 <jameelibrahim107@gmail.com>
2023-08-04 10:59:14 -0400

    lifting state

--- src/Components/Attendees.jsx
+++ src/Components/Attendees.jsx
@@ -1,3 +1,61 @@
-export default function Attendees() {
-  return;
+import React, { useState } from "react";
+
+export default function Attendees({ attendees, updateEventAttendance, event}) {
+
+  const [showAttendees, setShowAttendees] = useState(false);
+
+  function toggleEventAttendees() {
+    setShowAttendees(!showAttendees);
+  }
+
+  return(
+    <>
+       <button onClick={toggleEventAttendees}>
+                        {!showAttendees ? "Show Attendees" : "Hide Attendees"}
+                      </button>
+
+                      {showAttendees ? (
+                        <div className="attendees">
+                          {attendees.map((attendee, index) => (
+                            <React.Fragment key={index}>
+                              <div key={attendee.id} className="attendee">
+                                <p>
+                                  <img
+                                    src={attendee.avatar}
+                                    alt={attendee.firstName}
+                                  />
+                                  {"   "}
+                                  <span>
+                                    {" "}
+                                    {attendee.firstName} {attendee.lastName}{" "}
+                                  </span>
+                                </p>
+                                <p>
+                                  <button
+                                    className="clickable"
+                                    onClick={() =>
+                                      updateEventAttendance(
+                                        event.id,
+                                        attendee.id
+                                      )
+                                    }
+                                  >
+                                    Attending:
+                                  </button>
+                                  <span>
+                                    {attendee.attendance ? "✅" : "❌"}
+                                  </span>
+                                </p>
+
+                                <p>
+                                  <span>Note:</span> {attendee.note}
+                                </p>
+                              </div>
+                            </React.Fragment >
+                          ))}
+                        </div>
+                      ) : null}
+                      
+  </>
+  );
 }
--- src/Components/Event.jsx
+++ src/Components/Event.jsx
@@ -1,3 +1,23 @@
-export default function Event() {
-  return;
+import Attendees from "./Attendees"
+
+export default function Event( { event, attendees, updateEventAttendance }) {
+  return(
+    <li key={event.id}>
+                    <img src={event.eventImage} alt={event.name} />
+                    <h5>
+                      {event.name} {event.eventType}
+                    </h5>
+                    <br />
+                    <span>Organized by: {event.organizer} </span>
+                    <br />
+                    <>
+                      <Attendees
+                         attendees={attendees}
+                         updateEventAttendance={updateEventAttendance}
+                         event={event}
+                      
+                      />
+                    </>
+                  </li>
+  )
 }
